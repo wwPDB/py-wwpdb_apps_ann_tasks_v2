4655bdcb1d321df66bd108242d3aaef3e9da3e88
jturner <jturner@ebi.ac.uk>
2022-02-04 16:24:26 +0000

    Troubleshooting
    Signed-off-by: jturner <jturner@ebi.ac.uk>

--- wwpdb/apps/ann_tasks_v2/webapp/CommonTasksWebAppWorker.py
+++ wwpdb/apps/ann_tasks_v2/webapp/CommonTasksWebAppWorker.py
@@ -2278,16 +2278,16 @@ class CommonTasksWebAppWorker(WebAppWorkerBase):
         for data_file in data_files:
             ok = du.fetchId(entryId, contentType=data_file[0], formatType=data_file[1], fileSource=fileSource,
                     instance=instance, partNumber=data_file[2])
+            if ok:
+                downloadPath = du.getDownloadPath()
+                downloadPath = du.getWebPath()
+                url_name = '{}_{}_url'.format(data_file[0].replace('-', '_'), data_file[2])
+                myD.setdefault('molStar-display-objects', []).append('{}="{}"'.format(url_name, downloadPath))
 
-            downloadPath = du.getDownloadPath()
-            downloadPath = du.getWebPath()
-            url_name = '{}_{}_url'.format(data_file[0].replace('-', '_'), data_file[2])
-            myD.setdefault('molStar-display-objects', []).append('{}="{}"'.format(url_name, downloadPath))
-
-            if data_file[3]:
-                contourMap = '{}_contourLevel'.format(data_file[0].replace('-', '_'))
-                myD.setdefault('molStar-display-objects', []).append(
-                    '{}={}'.format(contourMap, float(data_file[3])))
+                if data_file[3]:
+                    contourMap = '{}_contourLevel'.format(data_file[0].replace('-', '_'))
+                    myD.setdefault('molStar-display-objects', []).append(
+                        '{}={}'.format(contourMap, float(data_file[3])))
 
         # EM image
 
