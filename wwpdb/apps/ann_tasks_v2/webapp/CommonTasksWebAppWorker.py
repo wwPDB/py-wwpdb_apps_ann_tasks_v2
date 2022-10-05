1d9ed2185bfb8607cab2a09b8a83d54846bf2e29
jturner <jturner@ebi.ac.uk>
2022-10-05 10:17:40 +0100

    troubleshooting
    Signed-off-by: jturner <jturner@ebi.ac.uk>

--- wwpdb/apps/ann_tasks_v2/webapp/CommonTasksWebAppWorker.py
+++ wwpdb/apps/ann_tasks_v2/webapp/CommonTasksWebAppWorker.py
@@ -2995,16 +2995,17 @@ class CommonTasksWebAppWorker(WebAppWorkerBase):
 
                 # Convert map files to bcif files when header updated
                 pR = ProcessRunner(verbose=self.__verbose, log=self.__lfh)
+                '''
                 pR.setInput("src", wfoInp)
                 op = "em-volume-bcif-conversion"
-'''
+
                 #
                 ok = pR.setAction(op)
                 self.__lfh.write("setAction() for %s returns status %r\n" % (op, ok))
                 ok = pR.preCheck()
                 self.__lfh.write("preCheck() for %s returns status %r\n" % (op, ok))
                 ok = pR.run()
-'''
+                '''
             #AnnotationUtils().emVolumeBcifConversionOp(inputObjectD={'src': wfoInp}, outputObjectD={}, userParameterD={}, internalParameterD={})
 
             except:  # noqa: E722 pylint: disable=bare-except
