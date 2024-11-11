6a819aba9478794dac20d00e4b7d2c99dd40a9b7
Ezra Peisach <ezra.peisach@rcsb.org>
2024-11-11 09:50:48 -0500

    PEP8 cleanup

--- wwpdb/apps/ann_tasks_v2/utils/MergeXyz.py
+++ wwpdb/apps/ann_tasks_v2/utils/MergeXyz.py
@@ -77,7 +77,7 @@ class MergeXyz(SessionWebDownloadUtils):
             inpPath = os.path.join(self.__sessionPath, inpFile)
             logPath1 = os.path.join(self.__sessionPath, entryId + "-merge-xyz.log")
             retPath = os.path.join(self.__sessionPath, entryId + "_model-updated_P1.cif")
-            for filePath in ( retPath, logPath1 ):
+            for filePath in (retPath, logPath1):
                 if os.access(filePath, os.R_OK):
                     os.remove(filePath)
                 #
